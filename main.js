c32dd2313b82827f4612db449c3612e496eb567e
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2022-08-07 14:13:45 +0200

    Add features

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const { BrowserWindow, app, dialog } = require("electron");
+const {
+  BrowserWindow,
+  app,
+  dialog,
+  Notification,
+  MenuItem,
+  Menu,
+} = require("electron");
 const { ipcMain } = require("electron/main");
 const path = require("path");
 const fs = require("fs");
 require("electron-reloader")(module);
 
+const isMac = process.platform === "darwin";
+
 /** @type BrowserWindow */
 let mainWindow;
+
+let openFilePath = "";
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({
@@ -14,17 +25,77 @@ const createWindow = () => {
     titleBarStyle: "hiddenInset",
     webPreferences: {
       preload: path.join(app.getAppPath(), "renderer.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
     },
+    title: "editor",
   });
 
+  /** @type MenuItem[] */
+  const menuTemplate = [
+    ...(isMac
+      ? [
+          {
+            label: app.name,
+            submenu: [
+              { role: "about" },
+              { type: "separator" },
+              { role: "services" },
+              { type: "separator" },
+              { role: "hide" },
+              { role: "hideOthers" },
+              { role: "unhide" },
+              { type: "separator" },
+              { role: "quit" },
+            ],
+          },
+        ]
+      : []),
+    {
+      label: "File",
+      submenu: [
+        {
+          label: "New",
+          click: () => ipcMain.emit("new-doc-triggered"),
+        },
+        {
+          label: "Open",
+          click: () => ipcMain.emit("open-doc-triggered"),
+        },
+        { role: "separator" },
+        {
+          label: "Open Recent",
+          role: "recentdocuments",
+          submenu: [
+            {
+              label: "Clear Recent",
+              role: "clearrecentdocuments",
+            },
+          ],
+        },
+        {
+          role: "quit",
+        },
+      ],
+    },
+    { role: "editMenu" },
+  ];
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
   mainWindow.webContents.openDevTools();
   mainWindow.loadFile("index.html");
 };
 
+function handleError() {
+  new Notification({
+    title: "Error",
+    body: "Sorry, something went wrong",
+  }).show();
+}
+
 app.whenReady().then(createWindow);
 
 ipcMain.on("new-doc-triggered", () => {
-  console.log("WERKT");
   dialog
     .showSaveDialog(mainWindow, {
       filters: [{ name: "text file", extensions: ["txt"] }],
@@ -32,10 +103,41 @@ ipcMain.on("new-doc-triggered", () => {
     .then(({ filePath }) => {
       fs.writeFile(filePath, "", err => {
         if (err) {
-          console.error(err);
+          handleError();
         } else {
+          app.addRecentDocument(filePath);
+          openFilePath = filePath;
           mainWindow.webContents.send("document-created", filePath);
         }
       });
     });
+});
+
+ipcMain.on("open-doc-triggered", () => {
+  dialog
+    .showOpenDialog({
+      properties: ["openFile"],
+      filters: [{ name: "text file", extensions: ["txt"] }],
+    })
+    .then(({ filePaths }) => {
+      const filePath = filePaths[0];
+
+      fs.readFile(filePath, "utf-8", (err, data) => {
+        if (err) {
+          handleError();
+        } else {
+          app.addRecentDocument(filePath);
+          openFilePath = filePath;
+          mainWindow.webContents.send("document-opened", { filePath, data });
+        }
+      });
+    });
+});
+
+ipcMain.on("file-content-updated", (_, content) => {
+  fs.writeFile(openFilePath, content, err => {
+    if (err) {
+      handleError();
+    }
+  });
 });
--- renderer.js
+++ renderer.js
@@ -1,14 +1,42 @@
 const { ipcRenderer } = require("electron/renderer");
+const path = require("path");
 
 window.addEventListener("DOMContentLoaded", () => {
   const newBtn = document.getElementById("new-btn");
   const openBtn = document.getElementById("open-btn");
+  const docName = document.getElementById("doc-name");
+  const editedText = document.getElementById("edited-text");
 
   newBtn.addEventListener("click", () => {
     ipcRenderer.send("new-doc-triggered");
   });
 
+  openBtn.addEventListener("click", () => {
+    ipcRenderer.send("open-doc-triggered");
+  });
+
+  editedText.addEventListener("input", e => {
+    ipcRenderer.send("file-content-updated", e.target.value);
+  });
+
+  /**
+   *
+   * @param {string} filePath
+   * @param {string} documentContent
+   * @returns {void}
+   */
+  function handleDocumentChange(filePath, documentContent = "") {
+    docName.innerHTML = path.parse(filePath).base;
+    editedText.removeAttribute("disabled");
+    editedText.value = documentContent;
+    editedText.focus();
+  }
+
   ipcRenderer.on("document-created", (_, filePath) => {
-    console.log(filePath);
+    handleDocumentChange(filePath);
+  });
+
+  ipcRenderer.on("document-opened", (_, { filePath, data }) => {
+    handleDocumentChange(filePath, data);
   });
 });
